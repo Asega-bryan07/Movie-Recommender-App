8abd112971ac834e5a4b77e277e085b7dcef3fc4
BRYAN ASEGA <111067639+Asega-bryan07@users.noreply.github.com>
2024-02-17 17:59:31 +0300

    Update movie_rec_model.py

--- movie_rec_model.py
+++ movie_rec_model.py
@@ -22,8 +22,8 @@ import pickle
 ```MovieDB API:```
 """
 
-credits_data = '1GW9YdU8Bo9xkdYjvijm9yIBdsDce4tmR'
-movies_data = '1dd5EHukYw5nQKrsFLCkqktXSGRoD0C8m'
+credits_data = os.environ.get("CREDITS_DATA")
+movies_data = os.environ.get("MOVIES_DATA")
 
 # Get the direct download link
 url1 = f'https://drive.google.com/uc?id={credits_data}'
