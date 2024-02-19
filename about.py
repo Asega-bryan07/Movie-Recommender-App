956468f858151186ae6c52fa5cc50090bfc22d3d
BRYAN ASEGA <111067639+Asega-bryan07@users.noreply.github.com>
2024-02-19 22:57:36 +0300

    Update about.py

--- about.py
+++ about.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import streamlit as st
 
 def app():
@@ -12,19 +11,4 @@ def app():
     st.markdown('Contact via mail: [Almasibryan7@gmail.com@gmail.com]')
 
 if __name__ == '__main__':
-=======
-import streamlit as st
-
-def app():
-    st.subheader('The Movie Recommender App was developed purposely for movie recommendations.')
-    st.subheader('A user of the app has the ability to prompt for a movie of choice. However, \
-               Multiple genres cannot be selected at the moment. This is an improvement that will take\
-               shape in the next few days.\n\
-               Enjoy the app. Cheers! :)\n\
-               To access the codes and deployment pipeline, please visit my github page: ')
-    st.markdown('Created by: [ENG. ASEGA BRYAN](https://github.com/asega-bryan07/movie-recommender-app)')
-    st.markdown('Contact via mail: [Almasibryan7@gmail.com@gmail.com]')
-
-if __name__ == '__main__':
->>>>>>> 033a69db2e971c251caf17c887a1b47af987001c
     app()
